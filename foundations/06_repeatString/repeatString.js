ac47f2f332d422773446a70a7c9ef3a43e0dc185
Airuz Balungay <balungay.airuz@gmail.com>
2026-07-22 09:46:00 +0800

    finished 6 - 9 js exercises in foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    let i = 0;
 
+    while (i <= repeat){
+        let outputString = '';
+        outputString += string;
+    }
+    return outputString;
 };
+
+repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
