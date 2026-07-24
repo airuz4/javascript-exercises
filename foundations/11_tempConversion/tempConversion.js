56a48b9cb98b4aa4a4f1e5639a36be3b4259c515
Airuz Balungay <balungay.airuz@gmail.com>
2026-07-24 10:30:23 +0800

    finished exercise 11 from foundations, used Number

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let newCelcius = (fah  - 32) * (5/9);
+
+  if (newCelcius % 1 != 0){
+    return Number(newCelcius.toFixed(1));
+  }
+  return newCelcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let newFah = (celcius * (9/5) + 32);
+  if (newFah % 1 != 0){
+    return Number(newFah.toFixed(1));
+  }
+  return newFah;
 };
 
 // Do not edit below this line
